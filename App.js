e0401d1bed8cc99aea4a20c4111572b2dde15f52
GaryRoberts <garyroberts1995@gmail.com>
2021-12-20 17:22:35 -0500

    fixed bug

--- App.js
+++ App.js
@@ -1,44 +1,50 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet,Image,ImageBackground,TouchableOpacity,Text,TextInput, View } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-web';
 
-export default class App extends React.Component {
-  state={
-    email:"",
-    password:""
-  }
-  render(){
-    return (
-      <View style={styles.container}>
-        <Text style={styles.logo}>HeyAPP</Text>
-        <View style={styles.inputView} >
+const image = { uri: "https://img.wallpapic.com/i4172-129-933/medium/beautiful-sky-nature-mountains-clouds-wallpaper.jpg" };
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+       <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+     <Image
+        source={{
+          uri: 'https://digitalasset.intuit.com/IMAGE/A9pgeoJgK/Taxcaster.png'
+        }}
+        style={{ width: 100, height: 100,justifyContent: 'center',marginBottom:30}}
+      />
+       <View style={styles.inputView} >
           <TextInput  
             style={styles.inputText}
-            placeholder="Email..." 
-            placeholderTextColor="#003f5c"
+            placeholder="Email" 
+            placeholderTextColor="white"
             onChangeText={text => this.setState({email:text})}/>
         </View>
         <View style={styles.inputView} >
           <TextInput  
-            secureTextEntry
             style={styles.inputText}
-            placeholder="Password..." 
-            placeholderTextColor="#003f5c"
+            placeholder="Password" 
+            placeholderTextColor="white"
             onChangeText={text => this.setState({password:text})}/>
         </View>
+
         <TouchableOpacity>
           <Text style={styles.forgot}>Forgot Password?</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>LOGIN</Text>
-        </TouchableOpacity>
+
         <TouchableOpacity>
           <Text style={styles.loginText}>Signup</Text>
         </TouchableOpacity>
 
-  
-      </View>
-    );
-  }
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+
+        </ImageBackground>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -54,6 +60,14 @@ const styles = StyleSheet.create({
     color:"#fb5b5a",
     marginBottom:40
   },
+
+   image: {
+    flex: 1,
+    width:"100%",
+    alignItems: 'center',
+    justifyContent: "center"
+  },
+
   inputView:{
     width:"80%",
     backgroundColor:"#465881",
@@ -61,16 +75,25 @@ const styles = StyleSheet.create({
     height:50,
     marginBottom:20,
     justifyContent:"center",
-    padding:20
+    padding:20,
+    color:"white",
   },
+
+  loginText:{
+    fontWeight: "bold",
+    color:"white",
+  },
+
   inputText:{
     height:50,
     color:"white"
   },
+
   forgot:{
     color:"white",
     fontSize:11
   },
+
   loginBtn:{
     width:"80%",
     backgroundColor:"#fb5b5a",
@@ -81,7 +104,10 @@ const styles = StyleSheet.create({
     marginTop:40,
     marginBottom:10
   },
-  loginText:{
-    color:"white"
-  }
 });
+
+const state={
+  email:"",
+  password:""
+}
+
